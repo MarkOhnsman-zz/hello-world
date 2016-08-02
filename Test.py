294ec9bc52d647d4aa73796ab6003fd8de7722d5
MarkOhnsman <MarkOhnsman@users.noreply.github.com>
2016-08-02 11:53:09 -0600

    ask on color

--- Test.py
+++ Test.py
@@ -1,3 +1,6 @@
 print "hello world, who is this?"
 name = user_input(">")
 print "hi %r!" % name
+print "What is your favorite color?"
+color = user_input (">")
+print "%r is a great color!"  % color
